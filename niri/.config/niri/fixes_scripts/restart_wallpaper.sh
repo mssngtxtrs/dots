31a77debdd7b20bae3823da145653b143cf32f12
mssngtxtrs <missingtextures970@gmail.com>
2026-03-13 13:33:19 +0300

    tweaked keybinds, added wallpaper restart script

---
+++ niri/.config/niri/fixes_scripts/restart_wallpaper.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+killall swww-daemon
+swww-daemon >/dev/null 2>&1 & disown
+notify-send "Wallpaper restart" "swww restarted succesfully! Waiting for swaybg..."
+
+
+while true; do
+    OUTPUT=$(swww query | grep -o '[^/]*$' 2>/dev/null | head -n 1)
+
+    if [[ -z "$OUTPUT" ]]; then
+        echo "OUTPUT is empty, waiting..."
+        sleep 1
+    elif [[ $OUTPUT =~ ^[^/]+\.[a-zA-Z0-9]+$ ]]; then
+        killall swaybg
+        swaybg --image "/home/mssngtxtrs/.cache/wallpapers/overview/$OUTPUT" --mode fill >/dev/null 2>&1 & disown
+        notify-send "Wallpaper restart" "swaybg restarted succesfully!"
+        
+        break
+    else
+        echo "OUTPUT is not matching, waiting..."
+        sleep 1
+    fi
+done
